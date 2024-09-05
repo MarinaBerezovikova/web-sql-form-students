daeaf96d5e499bdf0808f41a230309ee63da7f2c
master <marberezovikova@gmail.com>
2024-09-05 15:19:03 +0300

    create StudentFormServlet create student_db create web.xml create DataSourceFactory upd pom.xml

---
+++ src/main/java/com/example/DataSourceFactory.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import javax.sql.DataSource;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+public class DataSourceFactory {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/student_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "inmoodformysql";
+
+        private static final HikariDataSource dataSource;
+
+        static {
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(URL);
+            config.setUsername(USER);
+            config.setPassword(PASSWORD);
+            config.setMaximumPoolSize(10);
+
+            dataSource = new HikariDataSource(config);
+        }
+
+        public static DataSource getDataSource() {
+            return dataSource;
+        }
+    }
---
+++ src/main/java/com/example/StudentFormServlet.java
@@ -0,0 +1,41 @@
+package com.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class StudentFormServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try (Connection connection = DataSourceFactory.getDataSource().getConnection()) {
+            String sql = "SELECT * FROM student_db";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                // Обработка результата
+                System.out.println("Имя: " + resultSet.getString("имя"));
+            }
+
+            resultSet.close();
+            statement.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String firstName = request.getParameter("firstName");
+        String courseNumber = request.getParameter("courseNumber");
+
+        // код для сохранения студента в базу данных
+
+
+    }
+}
